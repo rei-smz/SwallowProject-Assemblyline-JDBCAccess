c68fba792500fc84ad130d9bd41998367ba8c904
d10sama <d10sama@bupt.edu.cn>
2021-02-05 14:41:14 +0800

    Update sample1_query.java

--- src/main/java/com/example/demo/Controller/sample1_query.java
+++ src/main/java/com/example/demo/Controller/sample1_query.java
@@ -20,10 +20,10 @@ import java.util.Map;
 @Controller
 @CrossOrigin
 public class sample1_query {
-
     @JsonProperty("id")
     private static final String template = "select * from sample1 where id=%d";
     private static int ssn=1;
+    private static int ssn2=1;
     private static long rows;
     @Autowired
     @Qualifier("secondaryJdbcTemplate")
@@ -43,5 +43,12 @@ public class sample1_query {
     public long rows() {
         String sentence="select table_rows from tables where table_name=\"sample1\";";
         return jdbcTemplate2.queryForObject(sentence,long.class);
+    }
+    @RequestMapping("/function1")
+    @ResponseBody
+    public String fun1() {
+        String fun1="y=ax+b,a=2,b=3,result = %f";
+        double result1=jdbcTemplate1.queryForObject(String.format("select length from sample1 where id=%d",ssn2++),double.class);
+        return String.format(fun1,(result1*2+3));
     }
 }
