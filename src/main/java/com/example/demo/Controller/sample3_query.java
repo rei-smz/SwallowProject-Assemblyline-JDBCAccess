c2f4a9aeee238470c8b0895493b6d9cb59aa1ada
d10sama <d10sama@bupt.edu.cn>
2021-02-24 12:13:33 +0800

    fix: @CrossOrigin

--- src/main/java/com/example/demo/Controller/sample3_query.java
+++ src/main/java/com/example/demo/Controller/sample3_query.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +16,7 @@ import java.util.Map;
 
 @ResponseBody
 @Controller
+@CrossOrigin
 public class sample3_query {
 
 
